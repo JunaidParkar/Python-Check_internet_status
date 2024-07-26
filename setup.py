fb60c42e041b2b067b7be6c9e33f9c65524ff143
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2024-07-27 00:41:10 +0530

    Project init

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A python internet status checking library',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/JunaidParkar/Python-TTS',
+    url='https://github.com/JunaidParkar/Python-Check_internet_status/',
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
@@ -20,8 +20,8 @@ setup(
     ],
     python_requires='>=3.6',
     project_urls={
-        'Documentation': 'https://github.com/JunaidParkar/Python-TTS',
-        'Source': 'https://github.com/JunaidParkar/Python-TTS',
-        'Tracker': 'https://github.com/JunaidParkar/Python-TTS',
+        'Documentation': 'https://github.com/JunaidParkar/Python-Check_internet_status/',
+        'Source': 'https://github.com/JunaidParkar/Python-Check_internet_status/',
+        'Tracker': 'https://github.com/JunaidParkar/Python-Check_internet_status/',
     },
 )
